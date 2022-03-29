2e8c7215a24b4143e4fd24057ea792cda7dcfeb6
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2022-03-29 19:11:52 +0000

    update

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-//useState
 import React, {useState} from 'react';
-import {View, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/header';
 import ListItem from './components/listItem';
+import AddItem from './components/addItem';
 import {uuid} from 'uuidv4';
 
 const App = () => {
@@ -18,9 +18,20 @@ const App = () => {
       return prevItems.filter(item => item.id != id);
     });
   };
+
+  const addItem = item => {
+    if (!item) {
+      Alert.alert('cannot enter empty text');
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text: item}, ...prevItems];
+      });
+    }
+  };
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
         renderItem={({item}) => (
